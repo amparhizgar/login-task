84006158d9cd9faaac4bbd1662557bbb4f791acf
AmirHossein Parhizgar <amparhizgar@gmail.com>
2022-12-05 15:59:37 +0330

    initial commit

---
+++ app/src/main/java/ir/amirhparhizgar/logintask/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.amirhparhizgar.logintask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

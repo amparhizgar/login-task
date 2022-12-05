366cc351af1c4c64b56009605252060a9aa86532
AmirHossein Parhizgar <amparhizgar@gmail.com>
2022-12-05 18:13:45 +0330

    Fixed fragment transition animations

--- app/src/main/java/ir/amirhparhizgar/logintask/ui/EnterPhoneFragment.kt
+++ app/src/main/java/ir/amirhparhizgar/logintask/ui/EnterPhoneFragment.kt
@@ -1,5 +1,9 @@
 package ir.amirhparhizgar.logintask.ui
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import ir.amirhparhizgar.logintask.R
 import ir.amirhparhizgar.logintask.databinding.FragmentEnterPhoneBinding
 
 class EnterPhoneFragment : ViewBindingFragment<FragmentEnterPhoneBinding>() {
@@ -7,4 +11,12 @@ class EnterPhoneFragment : ViewBindingFragment<FragmentEnterPhoneBinding>() {
     override fun getViewBindingInflater(): ViewBindingInflater<FragmentEnterPhoneBinding> =
         FragmentEnterPhoneBinding::inflate
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            btnSignIn.setOnClickListener {
+                findNavController().navigate(R.id.action_enterPhoneFragment_to_enterOTPFragment)
+            }
+        }
+    }
 }

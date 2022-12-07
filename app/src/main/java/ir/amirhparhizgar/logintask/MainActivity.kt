a2903a96370f195bdcc61c8b1de775c134874a05
Amir Hossein Parhizgar <amparhizgar@gmail.com>
2022-12-07 19:52:29 +0330

    Changed Icon and language

--- app/src/main/java/ir/amirhparhizgar/logintask/MainActivity.kt
+++ app/src/main/java/ir/amirhparhizgar/logintask/MainActivity.kt
@@ -1,13 +1,29 @@
 package ir.amirhparhizgar.logintask
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base.setAppLocale())
+    }
+
+    fun Context.setAppLocale(): Context {
+        val locale = Locale("fa", "IR")
+        Locale.setDefault(locale)
+        with(resources.configuration) {
+            setLocale(locale)
+            setLayoutDirection(locale)
+            return createConfigurationContext(this)
+        }
     }
 }
